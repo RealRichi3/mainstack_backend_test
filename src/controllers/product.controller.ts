d065750b5e393988defa8e1016b336a4da544d4c
RealRichi3 <molunorichie@gmail.com>
2023-11-30 13:13:53 +0100

    update response format for productController

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -5,17 +5,47 @@ import { NotFoundError } from "../utils/error";
 class ProductController {
     static async createProduct(req: Request, res: Response) {
         const product = await ProductModel.create(req.body);
-        res.status(201).json(product);
+        res.status(201).json({
+            status: 'success',
+            message: 'Product created succesfully',
+            data: {
+                product
+            }
+        });
     }
 
     static async getProduct(req: Request, res: Response) {
-        const product = await ProductModel.findById(req.params.productId);
+        const product = await ProductModel.findById(req.query.productId);
 
         if (!product) {
             throw new NotFoundError("Product not found");
         }
 
-        res.status(200).json(product);
+        res.status(200).json({
+            status: 'success',
+            message: 'Product fetched successfully',
+            data: {
+                product
+            }
+        });
+    }
+
+    static async getAllProducts(req: Request, res: Response) {
+        const { page = 1, limit = 10 } = req.query as unknown as {
+            page: number;
+            limit: number;
+        };
+        const skip = (page - 1) * limit;
+
+        const products = await ProductModel.find().skip(skip).limit(limit).exec();
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Products fetched successfully',
+            data: {
+                products
+            }
+        });
     }
 
     static async updateProduct(req: Request, res: Response) {
@@ -29,7 +59,13 @@ class ProductController {
             throw new NotFoundError("Product not found");
         }
 
-        res.status(200).json(product);
+        res.status(200).json({
+            status: 'success',
+            message: 'Product updated successfully',
+            data: {
+                product
+            }
+        });
     }
 
     static async deleteProduct(req: Request, res: Response) {
@@ -39,19 +75,11 @@ class ProductController {
             throw new NotFoundError("Product not found");
         }
 
-        res.status(204).send();
-    }
-
-    static async getAllProducts(req: Request, res: Response) {
-        const { page = 1, limit = 10 } = req.query as unknown as {
-            page: number;
-            limit: number;
-        };
-        const skip = (page - 1) * limit;
-
-        const products = await ProductModel.find().skip(skip).limit(limit).exec();
-
-        res.status(200).json(products);
+        res.status(200).send({
+            status: 'success',
+            message: 'Product deleted successfully',
+            data: null
+        });
     }
 }
 
