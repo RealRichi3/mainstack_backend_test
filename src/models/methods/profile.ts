17489ca23b8de163c529f716fbd0fa3f69175350
RealRichi3 <molunorichie@gmail.com>
2023-11-30 13:14:46 +0100

    modify profile util conditional check to allow createprofile

--- src/models/methods/profile.ts
+++ src/models/methods/profile.ts
@@ -17,15 +17,16 @@ export async function getProfile(
 ): Promise<IUserProfileDoc<UserRole>> {
     let userProfile: IUserProfileDoc<UserRole> | null = null;
 
-    switch (this.role) {
-        case UserRole.Admin:
-            userProfile = await AdminProfileModel.findOne({ user: this._id });
-        case UserRole.SuperAdmin:
-            userProfile = await SuperAdminProfileModel.findOne({ user: this._id });
-        case UserRole.EndUser:
-            userProfile = await EndUserProfileModel.findOne({ user: this._id });
+    if (this.role === 'SuperAdmin') {
+        userProfile = await SuperAdminProfileModel.findOne({ user: this._id });
+    } else if (this.role === 'Admin') {
+        userProfile = await AdminProfileModel.findOne({ user: this._id });
+    } else if (this.role === 'EndUser') {
+        userProfile = await EndUserProfileModel.findOne({ user: this._id });
+    } else {
+        throw new Error("Invalid user role");
     }
-
+    
     if (!userProfile) {
         throw new Error("User profile not found");
     }
@@ -39,31 +40,32 @@ export async function createProfile(
     let userProfile: IUserProfileDoc<UserRole>;
 
     const session = this.$session();
-    switch (this.role) {
-        case UserRole.Admin:
-            userProfile = session
-                ? await AdminProfileModel.create(
-                    [{ _id: new ObjectId(), user: this._id, role: this.role }],
-                    { session }
-                ).then((r) => r[0])
-                : await AdminProfileModel.create({ _id: new ObjectId(), user: this._id, role: this.role });
-        case UserRole.SuperAdmin:
-            userProfile = session
-                ? await SuperAdminProfileModel.create(
-                    [{ _id: new ObjectId(), user: this._id, role: this.role }],
-                    { session }
-                ).then((r) => r[0])
-                : await SuperAdminProfileModel.create({
-                    _id: new ObjectId(), user: this._id,
-                    role: this.role,
-                });
-        case UserRole.EndUser:
-            userProfile = session
-                ? await EndUserProfileModel.create(
-                    [{ _id: new ObjectId(), user: this._id, role: this.role }],
-                    { session }
-                ).then((r) => r[0])
-                : await EndUserProfileModel.create({ _id: new ObjectId(), user: this._id, role: this.role });
+    if (this.role === 'SuperAdmin') {
+        userProfile = session
+            ? await SuperAdminProfileModel.create(
+                [{ _id: new ObjectId(), user: this._id, role: this.role }],
+                { session }
+            ).then((r) => r[0])
+            : await SuperAdminProfileModel.create({
+                _id: new ObjectId(), user: this._id,
+                role: this.role,
+            });
+    } else if (this.role === 'Admin') {
+        userProfile = session
+            ? await AdminProfileModel.create(
+                [{ _id: new ObjectId(), user: this._id, role: this.role }],
+                { session }
+            ).then((r) => r[0])
+            : await AdminProfileModel.create({ _id: new ObjectId(), user: this._id, role: this.role });
+    } else if (this.role === 'EndUser') {
+        userProfile = session
+            ? await EndUserProfileModel.create(
+                [{ _id: new ObjectId(), user: this._id, role: this.role }],
+                { session }
+            ).then((r) => r[0])
+            : await EndUserProfileModel.create({ _id: new ObjectId(), user: this._id, role: this.role });
+    } else {
+        throw new Error("Invalid user role");
     }
 
     return userProfile;
