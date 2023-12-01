e8d8c672ce648c49f7482c06e0cf67695632d6c2
RealRichi3 <molunorichie@gmail.com>
2023-12-01 16:39:31 +0100

    update cache test

--- src/test/utils/cache.test.ts
+++ src/test/utils/cache.test.ts
@@ -1,9 +1,13 @@
 import CacheUtil from '../../utils/cache';
-import { describe, it } from 'mocha';
+import { describe, it, afterEach } from 'mocha';
 import { expect } from 'chai';
+import redisClient from '../../database/redis';
+import sinon from 'sinon';
 
 describe('Cache utility', () => {
-    afterEach(() => {
+    afterEach(async () => {
+        // Clean up keys used in tests
+        await redisClient.del('testKey', 'nonExistingKey');
     });
 
     describe('saveToCache', () => {
@@ -12,7 +16,10 @@ describe('Cache utility', () => {
             const value = { name: 'John' };
 
             await CacheUtil.saveToCache({ key, value });
-           
+
+            // Assert that the key exists in the cache
+            const result = await redisClient.get(key);
+            expect(result).to.equal(JSON.stringify(value));
         });
 
         it('should save value to cache with ttl', async () => {
@@ -20,7 +27,11 @@ describe('Cache utility', () => {
             const value = { name: 'John' };
             const ttl = 60;
 
-            await CacheUtil.saveToCache({ key, value, ttl })
+            await CacheUtil.saveToCache({ key, value, ttl });
+
+            // Assert that the key exists in the cache with a TTL
+            const result = await redisClient.ttl(key);
+            expect(result).to.be.lessThan(ttl + 1); // Allow some margin for TTL precision
         });
     });
 
@@ -29,8 +40,18 @@ describe('Cache utility', () => {
             const key = 'testKey';
             const expectedValue = JSON.stringify({ name: 'John' });
 
+            // Save a value to the cache for the test
+            await redisClient.set(key, expectedValue);
+
             const result = await CacheUtil.getFromCache(key);
             expect(result).to.equal(expectedValue);
+        });
+
+        it('should return null for non-existing key', async () => {
+            const key = 'nonExistingKey';
+
+            const result = await CacheUtil.getFromCache(key);
+            expect(result).to.equal(null);
         });
     });
 
@@ -38,10 +59,21 @@ describe('Cache utility', () => {
         it('should delete value from cache', async () => {
             const key = 'testKey';
 
+            // Save a value to the cache for the test
+            await redisClient.set(key, 'value');
+
             await CacheUtil.deleteFromCache(key);
 
-            const result = await CacheUtil.getFromCache(key);
-            expect(result).to.equal(null)
+            // Assert that the key no longer exists in the cache
+            const result = await redisClient.exists(key);
+            expect(result).to.equal(0);
+        });
+
+        it('should not throw error for non-existing key', async () => {
+            const key = 'nonExistingKey';
+
+            // Deleting a non-existing key should not throw an error
+            await CacheUtil.deleteFromCache(key);
         });
     });
 });
